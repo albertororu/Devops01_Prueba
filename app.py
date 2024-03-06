cb20c93b66b0aab707318bebf77aa9e553967333
albertororu <albertororu16@gmail.com>
2024-03-06 22:22:23 +0100

    corregido

--- app.py
+++ app.py
@@ -5,7 +5,5 @@ def suma(a: int, b: int) -> int:
     """ docstring function
     # nos debe dar errores en linter
     """
-    x = "no se usa"
-    return a+ b
-    x = "no se usa esta variable"
+    
     return a + b
